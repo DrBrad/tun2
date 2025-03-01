18c52978f40723af8ab47ae3c6957c1b61844448
DrBrad <brad@bradeagle.com>
2025-02-28 20:53:41 -0700

    Network handling working

--- src/main.rs
+++ src/main.rs
@@ -197,94 +197,9 @@ fn main() -> std::io::Result<()> {
         let packet = decode_packet(Interfaces::Ethernet, &buf);
 
 
-
-        /*
         let ethernet_frame = packet.get_frame().as_any().downcast_ref::<EthernetFrame>().unwrap();
         match ethernet_frame.get_type() {
-            Types::IPv4 => {
-                let ipv4_layer = ethernet_frame.get_data().unwrap().as_any().downcast_ref::<Ipv4Layer>().unwrap();
-
-                match ipv4_layer.get_protocol() {
-                    Protocols::HopByHop => {}
-                    Protocols::Icmp => {}
-                    Protocols::Igmp => {}
-                    Protocols::Tcp => {}
-                    Protocols::Udp => {
-                        let udp_layer = ipv4_layer.get_data().unwrap().as_any().downcast_ref::<UdpLayer>().unwrap();
-
-                        match udp_layer.get_payload() {
-                            UdpPayloads::Known(_type, payload) => {
-
-
-                                match _type {
-                                    UdpTypes::Dhcp => {
-                                        let dhcp_layer = payload.as_any().downcast_ref::<DhcpLayer>().unwrap();
-                                        println!("RECV {:?}", packet);
-
-                                        println!("DHCP");
-
-
-
-                                        let mut ethernet_frame_r = EthernetFrame::new(ethernet_frame.get_source_mac(), gateway_mac, Types::IPv4);
-                                        let mut ipv4_layer_r = Ipv4Layer::new(Ipv4Addr::new(10, 0, 0, 1), Ipv4Addr::new(255, 255, 255, 255), Protocols::Udp);
-                                        ipv4_layer_r.compute_checksum();
-                                        let mut udp_layer_r = UdpLayer::new(udp_layer.get_destination_port(), udp_layer.get_source_port());
-
-
-                                        let mut dhcp_layer_r = match DhcpMessageTypes::from_code(dhcp_layer.options[2]).unwrap() {
-                                            DhcpMessageTypes::Discover => {
-                                                generate_dhcp_offer(dhcp_layer, Ipv4Addr::new(10, 0, 0, 2), Ipv4Addr::new(10, 0, 0, 1))
-                                            }
-                                            DhcpMessageTypes::Request => {
-                                                generate_dhcp_ack(dhcp_layer, Ipv4Addr::new(10, 0, 0, 2), Ipv4Addr::new(10, 0, 0, 1))
-                                            }
-                                            _ => {
-                                                todo!()
-                                            }
-                                        };
-                                        //let mut dhcp_layer_r = generate_dhcp_offer(DhcpOperations::Offer, dhcp_layer, Ipv4Addr::new(10, 0, 0, 2), Ipv4Addr::new(10, 0, 0, 1));
-                                        dhcp_layer_r.compute_length();
-                                        udp_layer_r.set_payload_layer(UdpTypes::Dhcp, Box::new(dhcp_layer_r));
-
-                                        //udp_layer_r.compute_length();
-                                        udp_layer_r.compute_checksum(IpAddr::V4(ipv4_layer_r.get_source_address()), IpAddr::V4(ipv4_layer_r.get_destination_address()));
-                                        ipv4_layer_r.set_data(Box::new(udp_layer_r));
-
-
-                                        ipv4_layer_r.compute_checksum();
-                                        ethernet_frame_r.set_data(Box::new(ipv4_layer_r));
-
-
-
-                                        ethernet_frame_r.compute_length();
-
-                                        tunnel.write(&ethernet_frame_r.to_bytes())?;
-
-                                        //ethernet_frame_r.get_data().unwrap().as_any().downcast_ref::<Ipv4Layer>().unwrap().calculate_checksum();
-
-                                        println!("SENT {:?}", ethernet_frame_r);
-
-
-
-                                    }
-                                    _ => {}
-                                }
-
-
-
-                            }
-                            _ => {}
-                        }
-
-                    }
-                    Protocols::Ipv6 => {}
-                    Protocols::Gre => {}
-                    Protocols::Icmpv6 => {}
-                    Protocols::Ospf => {}
-                    Protocols::Sps => {}
-                }
-
-            }
+            Types::IPv4 => {}
             Types::Arp => {
                 let arp_layer = ethernet_frame.get_data().unwrap().as_any().downcast_ref::<ArpExtension>().unwrap();
 
@@ -317,7 +232,6 @@ fn main() -> std::io::Result<()> {
             Types::Broadcast => {}
         }
 
-        */
     }
 }
 
